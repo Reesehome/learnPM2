415e4438b6e28d6484494a622dde0923ac4769cb
Reese <reesegz@163.com>
2018-10-31 23:39:06 +0800

    1、修改pm2配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+    name: 'learnPM2',
+    script: './app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
@@ -19,12 +19,12 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'Administor',
+      host : '123.207.65.247',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      repo : 'git@github.com:Reesehome/learnPM2.git',
+      path : '/inetpub/wwwroot/learnPM2',
+      'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
