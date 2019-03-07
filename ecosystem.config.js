cbf2652445da21f0638fd11807ee5942a2e92d62
Reese <reesegz@163.com>
2019-03-07 23:36:25 +0800

    1、修改pm2配置文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
     script: './app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
     instances: 1,
     autorestart: true,
     watch: false,
